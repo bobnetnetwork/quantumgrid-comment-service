f0f108380aaf714c25f8378680265b76fe26bc95
Bobesz <bob@bobnet.network>
2024-08-30 21:42:06 +0200

    QUAN-36 Set up project structure using Spring Boot

---
+++ src/main/java/network/bobnet/quantumgrid/commnet_service/CommentServiceApplication.java
@@ -0,0 +1,16 @@
+package network.bobnet.quantumgrid.commnet_service;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Generated
+@SpringBootApplication
+public class CommentServiceApplication {
+
+	@Generated
+	public static void main(String[] args) {
+		SpringApplication.run(CommentServiceApplication.class, args);
+	}
+
+}
